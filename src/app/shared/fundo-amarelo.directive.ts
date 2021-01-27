78ff8b955a090743c96afcdab689ebbe7cfd0b3d
Calman <lucasalam@hotmail.com>
2021-01-27 17:24:37 -0300

    diretivas customizadas

---
+++ src/app/shared/fundo-amarelo.directive.ts
@@ -0,0 +1,15 @@
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: 'button[appFundoAmarelo]'
+})
+export class FundoAmareloDirective {
+
+  constructor(
+    private elementRef: ElementRef,
+    private renderer: Renderer2) { 
+    // errado pois é inseguro this.elementRef.nativeElement.style.backgroundColor = 'yellow';
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'yellow' );
+  }
+
+}
